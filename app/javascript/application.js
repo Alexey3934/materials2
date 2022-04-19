4e4f489c68e7574e082a1addf87df62d8b23ceff
Alll <Alll>
2022-04-19 22:54:50 +0300

    material

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,33 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 
+async function fn() {
+    const response = await fetch('/qqq');
+    const data     = await response.json()  
+
+    
+    const table = document.querySelector('#table')
+    for (const row of data) {
+       const trEl = document.createElement('tr')
+        const data_fields = [row.description,
+                            row.unit,
+                            row.price_retail,
+                            row.price_wholesale,
+                            row.amount_wholesale,
+                            row.company,
+                            '89891112233']  
+
+        for (const field of data_fields) {
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(field)
+            cell.appendChild(cellText)
+            cell.classList.add('td')
+            trEl.appendChild(cell);
+        
+        }
+
+
+        table.appendChild(trEl)
+    }
+}
+fn()
